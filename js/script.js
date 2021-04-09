52a079f8a671e4bf669ceb4dd5808bac024cbaca
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-09 16:54:44 +0200

    Aggiunto voto dinamico

--- js/script.js
+++ js/script.js
@@ -6,16 +6,25 @@ const app = new Vue ({
   },
   methods: {
     getMovies() {
-      axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: {
-          api_key: 'afbb7def2542bc20b7d5bfd24baf9b6a',
-          query: this.movieTitleInput,
-        }
-      })
-      .then( (arr) => {
-        this.moviesQuery = arr.data.results;
-        console.log(this.moviesQuery);
-      });
+      if ( this.movieTitleInput != '' ) {
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: 'afbb7def2542bc20b7d5bfd24baf9b6a',
+            query: this.movieTitleInput,
+          }
+        })
+        .then( (arr) => {
+          this.moviesQuery = arr.data.results;
+          this.convertVoteAvg();
+          console.log(this.moviesQuery);
+        });
+      }
     },
+    convertVoteAvg() {
+      this.moviesQuery.forEach( (movie) => {
+        const rating = Math.floor( movie.vote_average / 2 );
+        movie['rating'] = rating;
+      })
+    }
   }
 });
