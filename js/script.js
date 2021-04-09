076abd2342cafca51144306bd91add55c04f7ab9
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-09 13:26:17 +0200

    Struttura base
    -Manca css

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const app = new Vue ({
+  el: '#main-app',
+  data: {
+    moviesQuery: [],
+  },
+  methods: {
+    getMovies() {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          'api_key': 'afbb7def2542bc20b7d5bfd24baf9b6a',
+          'query': 'ciao'
+        }
+      })
+      .then( (arr) => {
+        this.moviesQuery = arr.data.results;
+        console.log(this.moviesQuery);
+      });
+    },
+  }
+});
