1a37e5d4156f26d1e1ab51c6ccfa4ba83d627958
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-09 16:10:53 +0200

    Aggiunta ricerca

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ const app = new Vue ({
   el: '#main-app',
   data: {
     moviesQuery: [],
+    movieTitleInput: '',
   },
   methods: {
     getMovies() {
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: 'afbb7def2542bc20b7d5bfd24baf9b6a',
-          query: 'ciao',
+          query: this.movieTitleInput,
         }
       })
       .then( (arr) => {
