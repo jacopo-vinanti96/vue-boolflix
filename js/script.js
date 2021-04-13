607c522c3543fe3b72ae129cc30d8e2055ee8209
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-13 15:08:49 +0200

    Ritocchi + Fix

--- js/script.js
+++ js/script.js
@@ -1,39 +1,47 @@
+// Documentazione API TMDB:
+// https://developers.themoviedb.org/3/search/search-companies
+
 const app = new Vue ({
-  el: '#main-app',
+  el: '#app',
   data: {
+    baseURL: 'https://api.themoviedb.org/3/search/',
+    lang: 'it-IT',
+    apiKey: 'afbb7def2542bc20b7d5bfd24baf9b6a',
     moviesQuery: [],
     tvSeriesQuery: [],
     TitleInput: '',
     moviePages: 0,
     tvPages: 0,
-    languages: ['en', 'de', 'es', 'fr', 'it', 'pt'],
+    flags: ['en', 'de', 'es', 'fr', 'it', 'pt'],
   },
   methods: {
     getMovies() {
       if ( this.TitleInput != '' ) {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
+        axios.get( this.baseURL + 'movie', {
           params: {
-            api_key: 'afbb7def2542bc20b7d5bfd24baf9b6a',
+            api_key: this.apiKey,
+            language: this.lang,
             query: this.TitleInput,
           }
         })
         .then( (arr) => {
           this.moviesQuery = arr.data.results;
           this.moviePages = arr.data.total_pages;
-          console.log(this.pages);
+          console.log(this.moviePages);
           this.convertVoteAvg(this.moviesQuery);
-          console.log(this.moviesQuery);
+          console.log(this.moviesQuery[0].overview.length);
         });
-        axios.get('https://api.themoviedb.org/3/search/tv', {
+        axios.get( this.baseURL + 'tv', {
           params: {
-            api_key: 'afbb7def2542bc20b7d5bfd24baf9b6a',
+            api_key: this.apiKey,
+            language: this.lang,
             query: this.TitleInput,
           }
         })
         .then( (arr) => {
           this.tvSeriesQuery = arr.data.results;
           this.tvPages = arr.data.total_pages;
-          console.log(this.pages);
+          console.log(this.tvPages);
           this.convertVoteAvg(this.tvSeriesQuery);
           console.log(this.tvSeriesQuery);
         });
@@ -41,7 +49,7 @@ const app = new Vue ({
     },
     convertVoteAvg(thisArr) {
       thisArr.forEach( (movie) => {
-        const rating = Math.floor( movie.vote_average / 2 );
+        const rating = Math.round( movie.vote_average / 2 );
         movie['rating'] = rating;
       })
     }
